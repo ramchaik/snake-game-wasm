b9793434ea61e694fa402c4d9c27a215de05df9a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2022-04-07 10:22:50 +0530

    feat: import fn to wasm

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    println!("Hello, {}!", name);
+    alert(name);
 }
 
+// Import function to WASM
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+// Packing to wasm
+// wasm-pack build --target web
--- www/index.js
+++ www/index.js
@@ -1,6 +1,10 @@
 import init, {greet} from "snake-game-wasm";
 
 init().then((_) => {
-    greet("Vaibhav");
-    console.log("OK");
+  // _ -> wasm instance (1st param) 
+  // can't directly call the function on wasm instance need glue code
+  // wasm can't understand string i/p i.e why we require glue code
+  // exported greet function is the glue code use that  
+  // _.greet("won't work");
+  greet("Vaibhav");
 });
