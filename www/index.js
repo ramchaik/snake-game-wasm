260393dc4da8371be16ff9afdc412fcc65db35ec
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2022-04-13 09:00:51 +0530

    feat(game): add world

--- www/index.js
+++ www/index.js
@@ -1,10 +1,6 @@
-import init, {greet} from "snake-game-wasm";
+import init, {World} from "snake-game-wasm";
 
 init().then((_) => {
-  // _ -> wasm instance (1st param) 
-  // can't directly call the function on wasm instance need glue code
-  // wasm can't understand string i/p i.e why we require glue code
-  // exported greet function is the glue code use that  
-  // _.greet("won't work");
-  greet("Vaibhav");
+  const world = World.new();
+  console.log(world.width)
 });
