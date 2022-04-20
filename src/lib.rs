d0ac87ee4213abe7c4a6e9a1c1469f34d4f6e456
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2022-04-20 09:24:33 +0530

    feat: create snake

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,41 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>
+}
+
+impl Snake {
+    pub fn new(spawn_index: usize) -> Snake {
+       Snake {
+           body: vec![SnakeCell(spawn_index)]
+       } 
+    }
+}
+
 #[wasm_bindgen]
 pub struct World {
-    width: usize
+    width: usize,
+    snake: Snake,
 }
 
 #[wasm_bindgen]
 impl World {
-   pub fn new() -> World {
-       World {
-           width: 8
-       }
-   } 
+    pub fn new() -> World {
+        World {
+            width: 8,
+            snake: Snake::new(10)
+        }
+    } 
 
    pub fn width(&self) -> usize {
        self.width
+   }
+
+   pub fn snake_head(&self) -> usize {
+       self.snake.body[0].0
    }
 }
 
