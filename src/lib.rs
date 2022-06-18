dfc5f0e1f6bacbe90db6fe0abca3a84be5debf72
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2022-06-18 23:28:33 +0530

    feat: add direction

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,30 @@
 use wasm_bindgen::prelude::*;
-use wee_alloc::WeeAlloc; 
+use wee_alloc::WeeAlloc;
 
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
+#[derive(PartialEq)]
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
 struct SnakeCell(usize);
 
 struct Snake {
-    body: Vec<SnakeCell>
+    body: Vec<SnakeCell>,
+    direction: Direction,
 }
 
 impl Snake {
     pub fn new(spawn_index: usize) -> Snake {
-       Snake {
-           body: vec![SnakeCell(spawn_index)]
-       } 
+        Snake {
+            body: vec![SnakeCell(spawn_index)],
+            direction: Direction::Right,
+        }
     }
 }
 
@@ -31,22 +41,30 @@ impl World {
         World {
             width,
             size: width * width,
-            snake: Snake::new(snake_index)
+            snake: Snake::new(snake_index),
         }
-    } 
+    }
 
-   pub fn width(&self) -> usize {
-       self.width
-   }
+    pub fn width(&self) -> usize {
+        self.width
+    }
 
-   pub fn snake_head_idx(&self) -> usize {
-       self.snake.body[0].0
-   }
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+    }
 
-   pub fn update(&mut self) {
-       let snake_idx = self.snake_head_idx();
-       self.snake.body[0].0 = (snake_idx + 1) % self.size;
-   }
+    pub fn update(&mut self) {
+        let snake_idx = self.snake_head_idx();
+        self.snake.body[0].0 = (snake_idx + 1) % self.size;
+
+        if self.snake.direction == Direction::Right {
+            self.snake.body[0].0 = (snake_idx + 1) % self.size;
+        }
+
+        if self.snake.direction == Direction::Left {
+            self.snake.body[0].0 = (snake_idx - 1) % self.size;
+        }
+    }
 }
 
 // Packing to wasm
