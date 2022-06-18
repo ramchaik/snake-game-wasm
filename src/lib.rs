602067c839835ccd2eb22aadf3ef97429044a657
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2022-06-18 23:54:08 +0530

    feat: add direction up/dow

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ impl Snake {
     pub fn new(spawn_index: usize) -> Snake {
         Snake {
             body: vec![SnakeCell(spawn_index)],
-            direction: Direction::Right,
+            direction: Direction::Up,
         }
     }
 }
@@ -56,15 +56,26 @@ impl World {
     pub fn update(&mut self) {
         let snake_idx = self.snake_head_idx();
         let row = snake_idx / self.width;
+        let col = snake_idx % self.width;
 
         if self.snake.direction == Direction::Right {
-            let next_col = (snake_idx + 1) % self.width;
+            let next_col = (col + 1) % self.width;
             self.snake.body[0].0 = (row * self.width) + next_col;
         }
 
         if self.snake.direction == Direction::Left {
-            let next_col = (snake_idx - 1) % self.width;
+            let next_col = (col - 1) % self.width;
             self.snake.body[0].0 = (row * self.width) + next_col;
+        }
+
+        if self.snake.direction == Direction::Up {
+            let next_row = (row - 1) % self.width;
+            self.snake.body[0].0 = (next_row * self.width) + col;
+        }
+
+        if self.snake.direction == Direction::Down {
+            let next_row = (row + 1) % self.width;
+            self.snake.body[0].0 = (next_row * self.width) + col;
         }
     }
 }
